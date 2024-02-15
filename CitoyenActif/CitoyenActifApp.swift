f11aa3ab65654df22b3df0122e08afa5533b27cd
komko <natacha.komko@uqtr.ca>
2024-02-14 20:26:11 -0500

    Initial Commit

---
+++ CitoyenActif/CitoyenActifApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitoyenActifApp.swift
+//  CitoyenActif
+//
+//  Created by Natacha Kom'ko on 2024-02-14.
+//
+
+import SwiftUI
+
+@main
+struct CitoyenActifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
