6b37abcd055c16af68f1849cb557bd5c7f741c6e
komko <natacha.komko@uqtr.ca>
2024-02-15 00:50:50 -0500

    SignInView

---
+++ CitoyenActif/SignInView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  CitoyenActif
+//
+//  Created by Natacha Kom'ko on 2024-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var email : String = ""
+    @State private var password : String = ""
+    @State private var wrongPassword = 0
+    @State private var wrongEmail = 0
+
+
+    
+    var body: some View {
+        
+            VStack {
+                Image("Image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200,height: 200)
+                    .cornerRadius(10)
+                    .padding()
+                
+                Text("Sign In")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.bottom, 5)
+
+                
+                Text("Sign in to your account")
+                    .foregroundColor(Color.black.opacity(0.6))
+                    .padding(.bottom, 10)
+                
+                TextField("Email",text: $email)
+                    .padding()
+                    .frame(width: 320,height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .border(.red,width: CGFloat(wrongEmail))
+                    .padding(.bottom, 10)
+                    .cornerRadius(10)
+                
+                SecureField("Password",text: $password)
+                    .padding()
+                    .frame(width: 320,height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .border(.red,width: CGFloat(wrongPassword))
+                    .padding(.bottom, 10)
+                    .cornerRadius(10)
+
+                
+                Button("Login"){
+                    
+                }
+                .foregroundColor(.white)
+                .frame(width: 320,height: 50)
+                .background(Color.blue)
+                .cornerRadius(10)
+                .padding(.bottom, 10)
+
+                Text("New user? Create Account.")
+                    .foregroundColor(Color.orange.opacity(1))
+                    .padding(.bottom, 20)
+                
+                Text("Forgot Password?")
+                    .foregroundColor(Color.blue.opacity(0.6))
+
+            }
+            .padding()
+        }
+    }
+
+
+#Preview {
+    ContentView()
+}
---
+++ CitoyenActif/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  CitoyenActif
+//
+//  Created by Natacha Kom'ko on 2024-02-15.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
