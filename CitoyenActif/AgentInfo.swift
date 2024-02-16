b8bbf18ca4ff890e928b77d343a2ea481789172d
William Junior <william.dema@yahoo.com>
2024-02-15 19:30:41 -0500

    Init Model citizen

---
+++ CitoyenActif/AgentInfo.swift
@@ -0,0 +1,8 @@
+//
+//  AgentInfo.swift
+//  CitoyenActif
+//
+//  Created by William Junior Demanou Fouelefack on 2024-02-15.
+//
+
+import Foundation
---
+++ CitoyenActif/models/CitoyenInfo.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  CitoyenActif
+//
+//  Created by William Junior Demanou Fouelefack on 2024-02-15.
+//
+
+import Foundation
+
+class CitoyenInfo{
+    
+    private 
+    /*
+     Les informations nom, prénom,
+
+     adresses, courriel, numéro de téléphone des citoyens sont nécessaires à la création de
+
+     leur compte
+     */
+}
