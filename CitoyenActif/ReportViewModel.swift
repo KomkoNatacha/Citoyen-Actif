26f2ffc5d74e8c3bbac593a1498a741f11e296b3
William Junior <william.dema@yahoo.com>
2024-03-04 01:06:30 -0500

    implementation des differentes page d'acceuil

---
+++ CitoyenActif/ReportViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ReportViewModel.swift
+//  CitoyenActif
+//
+//  Created by William Junior Demanou Fouelefack on 2024-03-02.
+//
+
+import Foundation
---
+++ CitoyenActif/StoreReport.swift
@@ -0,0 +1,8 @@
+//
+//  StoreReport.swift
+//  CitoyenActif
+//
+//  Created by William Junior Demanou Fouelefack on 2024-03-02.
+//
+
+import Foundation
